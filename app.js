548f9b81573b5e3651a215a881397ee900694d29
Pawel <zelep@o2.pl>
2025-06-09 21:30:40 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({path: './.env'});
+
+const app = express();
+
+const db = mysql.createConnection({
+    host:process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+     database: process.env.DATABASE,
+     charset:"utf8mb4",
+})
+
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+// console.log(__dirname);
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+app.set('view engine','hbs');
+
+// db.query(q,[values],(err,data)=>{
+//     if(err) return res.json(err);
+//     return res.json("Book has been created successfully");
+// })
+
+db.connect( (error)=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log("MYSQL Connected");
+    }
+})
+
+//Define Routes
+app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{console.log(`Server started on port ${port}`)})
